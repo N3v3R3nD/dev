2669e82dbb234c14cef5c79f11b1fd588cfff5da
N3v3R3nD <henning.midtbo@gmail.com>
2023-07-09 21:28:57 +0200

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,5 +1,7 @@
 import logging  # Import the logging module
 import h2o
+import numpy as np
+import pandas as pd
 from h2o.automl import H2OAutoML
 import config
 
@@ -12,26 +14,27 @@ kfold_splits = config.kfold_splits
 early_stopping_patience = config.early_stopping_patience
 
 def train_model(X_train, Y_train, X_test, Y_test, look_back, num_features, model_params):
+    # Reshape data if it's a 3D numpy array
+    if isinstance(X_train, np.ndarray) and len(X_train.shape) == 3:
+        X_train = X_train.reshape(X_train.shape[0], -1)
+    if isinstance(X_test, np.ndarray) and len(X_test.shape) == 3:
+        X_test = X_test.reshape(X_test.shape[0], -1)
+
     # Convert data to H2O data frames
-    X_train_h2o = h2o.H2OFrame(X_train if isinstance(X_train, pd.DataFrame) else X_train)
-    Y_train_h2o = h2o.H2OFrame(Y_train if isinstance(Y_train, pd.DataFrame) else Y_train)
-    X_test_h2o = h2o.H2OFrame(X_test if isinstance(X_test, pd.DataFrame) else X_test)
-    Y_test_h2o = h2o.H2OFrame(Y_test if isinstance(Y_test, pd.DataFrame) else Y_test)
+    X_train_h2o = h2o.H2OFrame(X_train if isinstance(X_train, pd.DataFrame) else X_train.tolist())
+    Y_train_h2o = h2o.H2OFrame(Y_train if isinstance(Y_train, pd.DataFrame) else Y_train.tolist())
+    X_test_h2o = h2o.H2OFrame(X_test if isinstance(X_test, pd.DataFrame) else X_test.tolist())
+    Y_test_h2o = h2o.H2OFrame(Y_test if isinstance(Y_test, pd.DataFrame) else Y_test.tolist())
 
     # Combine features and target into a single data frame
     train_data = X_train_h2o.cbind(Y_train_h2o)
     test_data = X_test_h2o.cbind(Y_test_h2o)
 
-    # Get the column names
-    x = train_data.columns
-    y = 'target'  # replace with your actual target column name
-
     # Print out the target column name and the list of column names
     print("Target column name: ", y)
     print("List of column names: ", x)
 
-    if y in x:
-        x.remove(y)
+    x.remove(y)
 
     # Run AutoML
     aml = H2OAutoML(max_models=20, seed=1)
