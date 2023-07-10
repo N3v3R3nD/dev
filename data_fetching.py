70f9c9b44bfa78994a74f6b5aac6c420fc082e3d
N3v3R3nD <henning.midtbo@gmail.com>
2023-07-10 13:54:41 +0200

    Update data_fetching.py

--- data_fetching.py
+++ data_fetching.py
@@ -23,7 +23,7 @@ def fetch_and_preprocess_data():
     # Fetch data using yfinance
     logging.info('Fetching data using yfinance')
     today = datetime.today().strftime('%Y-%m-%d')
-    data = yf.download(yfinance_symbol, start='1993-01-29', end=today)
+    data = yf.download(yfinance_symbol, start='2020-01-29', end=today)
     logging.info('Data downloaded from Yahoo Finance')
     logging.info(f'Data shape: {data.shape}')
     logging.info('First few rows of the data:')
