cc28dd0f3ad7b75c97f6c8625c6a2e3229016ca2
N3v3R3nD <henning.midtbo@gmail.com>
2023-07-10 17:38:35 +0200

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,87 +1,156 @@
-# model_training.py
-import logging 
+# data_fetching.py
+import logging
+from datetime import datetime
+
+import config
 import h2o
 import numpy as np
 import pandas as pd
-from h2o.automl import H2OAutoML
-import config
+import pandas_datareader as pdr
+import yfinance as yf
+from pandas.tseries.holiday import USFederalHolidayCalendar
+from sklearn.preprocessing import StandardScaler
 
-logging.basicConfig(level=logging.INFO)
 
+def fetch_and_preprocess_data():
 
-forecast_steps = config.forecast_steps
+    # Access the parameters
+    forecast_steps = config.forecast_steps
+    yfinance_symbol = config.yfinance_symbol
 
-def train_model(X_train, Y_train, X_test, Y_test, forecast_steps, num_features, model_params):
-    logging.info("Starting model training")
+    # Fetch data using yfinance
+    logging.info('Fetching data using yfinance')
+    today = datetime.today().strftime('%Y-%m-%d')
+    data = yf.download(yfinance_symbol, start='2020-01-29', end=today)
+    logging.info('Data downloaded from Yahoo Finance')
+    logging.info('Data shape: %s', data.shape)
+    logging.info('First few rows of the data:')
+
+    # Fetch macroeconomic data using FRED
+    logging.info('Fetching macroeconomic data using FRED')
+    start_date = '2020-01-29'
+    end_date = today
+    gdp = pdr.get_data_fred('GDP', start_date, end_date)
+    unemployment = pdr.get_data_fred('UNRATE', start_date, end_date)
+
+    # Preprocess macroeconomic data
+    logging.info('Preprocessing macroeconomic data')
+    gdp = gdp.resample('D').ffill()  # Fill missing values by forward filling
+    unemployment = unemployment.resample('D').ffill()  # Fill missing values by forward filling
+
+    # Merge macroeconomic data with existing data
+    logging.info('Merging macroeconomic data with existing data')
+    data = pd.merge(data, gdp, how='left', left_index=True, right_index=True)
+    data = pd.merge(data, unemployment, how='left', left_index=True, right_index=True)
+
+    # Engineer features from macroeconomic data
+    logging.info('Engineering features from macroeconomic data')
+    data['GDP Change'] = data['GDP'].pct_change()
+    data['Unemployment Change'] = data['UNRATE'].pct_change()
+
+    # Drop original macroeconomic columns
+    data = data.drop(columns=['GDP', 'UNRATE'])
+
+    # Add day of the week feature
+    data['DayOfWeek'] = data.index.dayofweek
+
+    # Add month of the year feature
+    data['Month'] = data.index.month
+
+    # Add is holiday feature
+    cal = USFederalHolidayCalendar()
+    holidays = cal.holidays(start=data.index.min(), end=data.index.max())
+    data['IsHoliday'] = data.index.isin(holidays).astype(int)
+
+    # Filter out non-business days
+    data = data[data.index.dayofweek < 5]
+
+    # Add technical indicators
+    data['SMA'] = data['Close'].rolling(window=14).mean()
+    data['EMA'] = data['Close'].ewm(span=14).mean()
+    delta = data['Close'].diff()
+    up = delta.clip(lower=0)
+    down = -1 * delta.clip(upper=0)
+    ema_up = up.ewm(com=13, adjust=False).mean()
+    ema_down = down.ewm(com=13, adjust=False).mean()
+    rs = ema_up / ema_down
+    data['RSI'] = 100 - (100 / (1 + rs))
+    exp1 = data['Close'].ewm(span=12, adjust=False).mean()
+    exp2 = data['Close'].ewm(span=26, adjust=False).mean()
+    macd = exp1 - exp2
+    signal = macd.ewm(span=9, adjust=False).mean()
+    data['MACD'] = macd - signal
+    data['MA20'] = data['Close'].rolling(window=20).mean()
+    data['20dSTD'] = data['Close'].rolling(window=20).std()
+    data['Upper'] = data['MA20'] + (data['20dSTD'] * 2)
+    data['Lower'] = data['MA20'] - (data['20dSTD'] * 2)
+    data['Cum_Daily_Returns'] = (data['Close'] / data['Close'].shift(1)) - 1
+    data['Cumulative_Returns'] = (1 + data['Cum_Daily_Returns']).cumprod()
+    data['VWAP'] = (data['Close'] * data['Volume']).cumsum() / data['Volume'].cumsum()
+    data = data.dropna()
+
+    # Select features
+    logging.info('Selecting features')
+    features = data[['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'SMA', 'EMA', 'RSI', 'MACD', 'Upper', 'Lower', 'Cumulative_Returns', 'VWAP', 'GDP Change', 'Unemployment Change']]
+    num_features = len(features.columns)  # Get the number of features
+    logging.info('Features: ' + str(features.columns.tolist()))  # Log the order of features
+
+    # Handle outliers
+    logging.info('Handling outliers')
+    Q1 = features.quantile(0.25)
+    Q3 = features.quantile(0.75)
+    IQR = Q3 - Q1
+    features = features[~((features < (Q1 - 1.5 * IQR)) | (features > (Q3 + 1.5 * IQR))).any(axis=1)]
+
+    # Preprocess data
+    logging.info('Preprocessing data')
+    feature_scaler = StandardScaler()
+    target_scaler = StandardScaler()
+
+    # Split data into training and test sets
+    logging.info('Splitting data into training and test sets')
+    split = int(0.8 * len(features))
+    train_features = features[:split]
+    test_features = features[split:]
+
+    # Fit the scaler on the training data and transform both training and test data
+    logging.info('Scaling data')
+    scaled_train_features = feature_scaler.fit_transform(train_features)
+    scaled_test_features = feature_scaler.transform(test_features)
+
+    # Do the same for the target variable
+    train_target = features[['Open']][:split]
+    test_target = features[['Open']][split:]
+    scaled_train_target = target_scaler.fit_transform(train_target)
+    scaled_test_target = target_scaler.transform(test_target)
+
+    # Create the dataset for training
+    logging.info('Creating dataset for training')
+    X_train, Y_train = [], []
+    forecast_steps = forecast_steps 
+    for i in range(forecast_steps, len(train_features)):
+        X_train.append(scaled_train_features[i-forecast_steps:i, :])
+        Y_train.append(scaled_train_target[i, 0])
+
+    # Create the dataset for testing
+    logging.info('Creating dataset for testing')
+    X_test, Y_test = [], []
+    for i in range(forecast_steps, len(test_features)):
+        X_test.append(scaled_test_features[i-forecast_steps:i, :])
+        Y_test.append(scaled_test_target[i, 0]) 
+
+    # Convert lists to numpy arrays
+    X_train, Y_train = np.array(X_train), np.array(Y_train)
+    X_test, Y_test = np.array(X_test), np.array(Y_test)
     
-    # Reshape data if it's a 3D numpy array
-    if isinstance(X_train, np.ndarray) and len(X_train.shape) == 3:
-        X_train = X_train.reshape(X_train.shape[0], -1)
-    if isinstance(X_test, np.ndarray) and len(X_test.shape) == 3:
-        X_test = X_test.reshape(X_test.shape[0], -1)
+    # Reshape the data to 2D
+    X_train_2d = X_train.reshape((X_train.shape[0], -1))
+    X_test_2d = X_test.reshape((X_test.shape[0], -1))
 
     # Convert data to H2O data frames
-    logging.info(f"Convert data to H2O data frames")
-    X_train_h2o = h2o.H2OFrame(X_train if isinstance(X_train, pd.DataFrame) else X_train.tolist())
-    Y_train_h2o = h2o.H2OFrame(Y_train if isinstance(Y_train, pd.DataFrame) else Y_train.tolist())
-    X_test_h2o = h2o.H2OFrame(X_test if isinstance(X_test, pd.DataFrame) else X_test.tolist())
-    Y_test_h2o = h2o.H2OFrame(Y_test if isinstance(Y_test, pd.DataFrame) else Y_test.tolist())
+    X_train_h2o = h2o.H2OFrame(X_train_2d)
+    Y_train_h2o = h2o.H2OFrame(Y_train)
+    X_test_h2o = h2o.H2OFrame(X_test_2d)
+    Y_test_h2o = h2o.H2OFrame(Y_test)
 
-    # Combine features and target into a single data frame
-    train_data = X_train_h2o.cbind(Y_train_h2o)
-    test_data = X_test_h2o.cbind(Y_test_h2o)
-
-    y = config.target_column_name  # Define the target column name
-    x = train_data.columns  # Define the feature column names
-    x.remove(y)
-
-    logging.info(f"Target column name: {y}")
-    logging.info(f"List of column names: {x}")
-
-    # Run AutoML
-    aml = H2OAutoML(max_models=1, seed=1)
-    aml.train(x=x, y=y, training_frame=train_data)
-
-    # Get the best model
-    model = aml.leader
-
-    # Make predictions
-    train_preds = model.predict(train_data)
-    test_preds = model.predict(test_data)
-
-    # Convert predictions to numpy array and flatten them
-    train_preds = train_preds.as_data_frame().values.flatten()
-    test_preds = test_preds.as_data_frame().values.flatten()
-
-    # Log the shapes for debugging
-    logging.debug(f'Shape of train_preds: {train_preds.shape}')
-    logging.debug(f'Shape of Y_train: {Y_train.shape}')
-    logging.debug(f'Shape of test_preds: {test_preds.shape}')
-    logging.debug(f'Shape of Y_test: {Y_test.shape}')
-
-    # Check that the shapes of the predictions are as expected
-    assert train_preds.shape == Y_train.shape, 'Unexpected shape of train_preds'
-    assert test_preds.shape == Y_test.shape, 'Unexpected shape of test_preds'
-
-    # Print or log the predictions
-    logging.info(f'Train predictions: {train_preds}')
-    logging.info(f'Test predictions: {test_preds}')
-
-    # Generate new input data for forecast
-    logging.debug(f'X_test shape: {X_test.shape}')  
-    logging.debug(f'forecast_steps: {forecast_steps}')  
-    forecast_input = X_test[-forecast_steps:]  # Get the most recent observations
-
-    # Flatten the forecast_input before converting to H2OFrame
-    forecast_input_flattened = forecast_input.reshape(-1, forecast_input.shape[-1])
-
-    # Convert forecast input to H2O data frame
-    forecast_input_h2o = h2o.H2OFrame(forecast_input_flattened)
-
-    # Make forecast
-    forecast = model.predict(forecast_input_h2o)
-
-    # Convert forecast to numpy array
-    forecast = forecast.as_data_frame().values.flatten()
-    logging.info("Model training completed")
-    return model, (train_preds, test_preds), forecast
+    return X_train, Y_train, X_test, Y_test, train_features, test_features, data, scaled_train_target, scaled_test_target, forecast_steps, target_scaler, num_features
