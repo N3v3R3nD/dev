a8145f00e02c6717c4510637288af37464351ec5
N3v3R3nD <henning.midtbo@gmail.com>
2023-07-10 13:54:21 +0200

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,3 +1,4 @@
+# model_training.py
 import logging 
 import h2o
 import numpy as np
@@ -12,6 +13,7 @@ forecast_steps = config.forecast_steps
 
 def train_model(X_train, Y_train, X_test, Y_test, forecast_steps, num_features, model_params):
     logging.info("Starting model training")
+    
     # Reshape data if it's a 3D numpy array
     if isinstance(X_train, np.ndarray) and len(X_train.shape) == 3:
         X_train = X_train.reshape(X_train.shape[0], -1)
@@ -28,10 +30,12 @@ def train_model(X_train, Y_train, X_test, Y_test, forecast_steps, num_features, 
     train_data = X_train_h2o.cbind(Y_train_h2o)
     test_data = X_test_h2o.cbind(Y_test_h2o)
 
-    # Print out the target column name and the list of column names
+    y = config.target_column_name  # Define the target column name
+    x = train_data.columns  # Define the feature column names
+    x.remove(y)
+
     logging.debug("Target column name: ", y)
     logging.debug("List of column names: ", x)
-    x.remove(y)
 
     # Run AutoML
     aml = H2OAutoML(max_models=20, seed=1)
@@ -41,16 +45,37 @@ def train_model(X_train, Y_train, X_test, Y_test, forecast_steps, num_features, 
     model = aml.leader
 
     # Make predictions
-    preds = model.predict(test_data)
+    train_preds = model.predict(train_data)
+    test_preds = model.predict(test_data)
 
-    # Convert predictions to numpy array
-    preds = preds.as_data_frame().values
+    # Convert predictions to numpy array and flatten them
+    train_preds = train_preds.as_data_frame().values.flatten()
+    test_preds = test_preds.as_data_frame().values.flatten()
+
+    # Log the shapes for debugging
+    logging.debug(f'Shape of train_preds: {train_preds.shape}')
+    logging.debug(f'Shape of Y_train: {Y_train.shape}')
+    logging.debug(f'Shape of test_preds: {test_preds.shape}')
+    logging.debug(f'Shape of Y_test: {Y_test.shape}')
+
+    # Check that the shapes of the predictions are as expected
+    assert train_preds.shape == Y_train.shape, 'Unexpected shape of train_preds'
+    assert test_preds.shape == Y_test.shape, 'Unexpected shape of test_preds'
+
+    # Print or log the predictions
+    logging.info(f'Train predictions: {train_preds}')
+    logging.info(f'Test predictions: {test_preds}')
 
     # Generate new input data for forecast
+    logging.debug(f'X_test shape: {X_test.shape}')  
+    logging.debug(f'forecast_steps: {forecast_steps}')  
     forecast_input = X_test[-forecast_steps:]  # Get the most recent observations
 
+    # Flatten the forecast_input before converting to H2OFrame
+    forecast_input_flattened = forecast_input.reshape(-1, forecast_input.shape[-1])
+
     # Convert forecast input to H2O data frame
-    forecast_input_h2o = h2o.H2OFrame(forecast_input if isinstance(forecast_input, pd.DataFrame) else forecast_input.tolist())
+    forecast_input_h2o = h2o.H2OFrame(forecast_input_flattened.tolist())
 
     # Make forecast
     forecast = model.predict(forecast_input_h2o)
@@ -58,4 +83,4 @@ def train_model(X_train, Y_train, X_test, Y_test, forecast_steps, num_features, 
     # Convert forecast to numpy array
     forecast = forecast.as_data_frame().values
     logging.info("Model training completed")
-    return model, preds, forecast
+    return model, (train_preds, test_preds), forecast
